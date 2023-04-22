03a23e9ccfd2ed3123574c890563a94a642762e5
dygma0 <404err@naver.com>
2023-04-23 08:20:07 +0900

    chore: 의존성 버저닝 및 업그레이드

--- perfume-core/build.gradle.kts
+++ perfume-core/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     // QueryDSL
     compileOnly("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
-    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api:2.1.1")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api:3.1.0")
 }
 
 tasks.bootJar {
