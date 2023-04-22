0df3397a5410897447bd0ba4f493f4986a0ebc6e
dygma0 <404err@naver.com>
2023-04-23 08:19:46 +0900

    chore: GraalVM 22.3.1 적용

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ### 프로젝트 자바 설치&적용
 
 ```shell
+jabba install 22.3.1-graalvm=tgz+https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.1/graalvm-ce-java19-darwin-aarch64-22.3.1.tar.gz
 jabba use
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
+            jvmTarget = "19"
         }
     }
 
