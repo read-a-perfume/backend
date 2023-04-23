c0d70b3a4e1674a820b051bbcca87f3766dda8e3
dygma0 <404err@naver.com>
2023-04-23 23:19:48 +0900

    refactor: entity scan 위치 변경

--- perfume-api/src/main/java/io/perfume/api/PerfumeApiApplication.java
+++ perfume-api/src/main/java/io/perfume/api/PerfumeApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("io.perfume.core")
 public class PerfumeApiApplication {
 
     public static void main(String[] args) {
