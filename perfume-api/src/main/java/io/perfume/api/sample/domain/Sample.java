a6c09883b6191d664b4d96c2c106f6cd53f9cdd8
dygma0 <404err@naver.com>
2023-05-04 23:59:11 +0900

    refactor: BaseEntity 상속 제거

--- perfume-api/src/main/java/io/perfume/api/sample/domain/Sample.java
+++ perfume-api/src/main/java/io/perfume/api/sample/domain/Sample.java
@@ -1,8 +1,7 @@
 package io.perfume.api.sample.domain;
 
 import io.perfume.api.base.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -14,14 +13,25 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class Sample extends BaseTimeEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Long id;
+
     @NotNull
     @Column(nullable = false)
     private String name;
 
     @Builder
     public Sample(@NotNull Long id, @NotNull String name, @NotNull LocalDateTime createdAt, LocalDateTime deletedAt) {
-        super(id, createdAt, deletedAt);
+        super(createdAt, deletedAt);
         this.name = name;
+        this.id = id;
+    }
+
+    @NotNull
+    public Long getId() {
+        return id;
     }
 
     @NotNull
