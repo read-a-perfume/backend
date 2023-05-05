aba143d7ee17b992c18c5bd9fffede8617455ee4
dygma0 <404err@naver.com>
2023-05-05 23:21:31 +0900

    feat: File domain class 추가
    Co-authored-by: oliviarla <imoliviarla@gmail.com>

---
+++ perfume-api/src/main/java/io/perfume/api/file/domain/File.java
@@ -0,0 +1,23 @@
+package io.perfume.api.file.domain;
+
+import io.perfume.api.base.BaseTimeDomain;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class File extends BaseTimeDomain {
+
+    private final Long id;
+
+    private final String url;
+
+    @Builder
+    private File(Long id, String url, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        super(createdAt, updatedAt, deletedAt);
+
+        this.id = id;
+        this.url = url;
+    }
+}
