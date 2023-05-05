3e84da9c91799b3427b67a93e7bb9cc0ec0aaa83
dygma0 <404err@naver.com>
2023-05-05 23:01:28 +0900

    feat: BaseTimeDomain 추가
    Co-authored-by: oliviarla <imoliviarla@gmail.com>

---
+++ perfume-api/src/main/java/io/perfume/api/base/BaseTimeDomain.java
@@ -0,0 +1,30 @@
+package io.perfume.api.base;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class BaseTimeDomain {
+
+    @NotNull
+    private final LocalDateTime createdAt;
+
+    @NotNull
+    private final LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public BaseTimeDomain(@NotNull LocalDateTime createdAt, @NotNull LocalDateTime updatedAt, LocalDateTime deletedAt) {
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public void markDelete(LocalDateTime now) {
+        this.deletedAt = now;
+    }
+}
