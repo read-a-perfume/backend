668bae7597bc78a9f735c958c0e41d2d8e2c745e
dygma0 <404err@naver.com>
2023-05-05 23:40:38 +0900

    refactor: BusinessRepository interface 분리
    Co-authored-by: oliviarla <imoliviarla@gmail.com>

--- perfume-api/src/main/java/io/perfume/api/business/infrastructure/persistence/BusinessJpaRepository.java
+++ perfume-api/src/main/java/io/perfume/api/business/infrastructure/persistence/BusinessJpaRepository.java
@@ -1,7 +1,6 @@
 package io.perfume.api.business.infrastructure.persistence;
 
-import io.perfume.api.business.application.port.out.BusinessRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BusinessJpaRepository extends JpaRepository<BusinessJpaEntity, Long>, BusinessRepository {
+public interface BusinessJpaRepository extends JpaRepository<BusinessJpaEntity, Long> {
 }
