cf99f8dcce4818b96d1deb0f81eff64355975d9e
dygma0 <404err@naver.com>
2023-05-05 22:09:07 +0900

    feat: File entity 선언
    Co-authored-by: oliviarla <imoliviarla@gmail.com>

---
+++ perfume-api/src/main/java/io/perfume/api/file/infrastructure/persistence/FileJpaEntity.java
@@ -0,0 +1,28 @@
+package io.perfume.api.file.infrastructure.persistence;
+
+import io.perfume.api.base.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.jetbrains.annotations.NotNull;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@ToString(onlyExplicitlyIncluded = true)
+public class FileJpaEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private Long id;
+
+    @NotNull
+    private String url;
+}
