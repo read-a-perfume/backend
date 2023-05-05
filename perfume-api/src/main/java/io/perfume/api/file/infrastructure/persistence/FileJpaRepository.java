0b344580ce2b895c6773c27e44f79827d8d4635f
dygma0 <404err@naver.com>
2023-05-05 23:17:35 +0900

    refactor: FileRepository interface 분리
    Co-authored-by: oliviarla <imoliviarla@gmail.com>

--- perfume-api/src/main/java/io/perfume/api/file/infrastructure/persistence/FileJpaRepository.java
+++ perfume-api/src/main/java/io/perfume/api/file/infrastructure/persistence/FileJpaRepository.java
@@ -1,9 +1,6 @@
 package io.perfume.api.file.infrastructure.persistence;
 
-import io.perfume.api.file.application.port.FileRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface FileJpaRepository extends JpaRepository<FileJpaEntity, Long>, FileRepository {
+public interface FileJpaRepository extends JpaRepository<FileJpaEntity, Long> {
 }
