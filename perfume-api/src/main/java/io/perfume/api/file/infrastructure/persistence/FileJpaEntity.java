e13a9a731eba43c971d5181e57c1f0a3cc815368
dygma0 <404err@naver.com>
2023-05-05 23:21:09 +0900

    feat: 설계 수정으로 인한 FileJpaEntity 리팩토링
    Co-authored-by: oliviarla <imoliviarla@gmail.com>

--- perfume-api/src/main/java/io/perfume/api/file/infrastructure/persistence/FileJpaEntity.java
+++ perfume-api/src/main/java/io/perfume/api/file/infrastructure/persistence/FileJpaEntity.java
@@ -1,20 +1,20 @@
 package io.perfume.api.file.infrastructure.persistence;
 
 import io.perfume.api.base.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 @Entity(name = "file")
+@Table(name = "file")
 @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @ToString(onlyExplicitlyIncluded = true)
+@Getter
 public class FileJpaEntity extends BaseTimeEntity {
 
     @Id
