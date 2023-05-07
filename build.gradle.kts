a20112250ae69a666762ab232347f32a39b2a248
oliviarla <imoliviarla@gmail.com>
2023-05-07 21:50:19 +0900

    feat: apply Spring Rest Docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
     id("org.graalvm.buildtools.native") version "0.9.21"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
     kotlin("plugin.jpa") version "1.8.21"
@@ -24,11 +25,13 @@ allprojects {
     repositories { mavenCentral() }
 }
 
+// common dependencies
 subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "kotlin-spring")
+    apply(plugin = "org.asciidoctor.jvm.convert")
 
     dependencies {
         // kotlin
@@ -41,6 +44,10 @@ subprojects {
 
         // test
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.6")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+        testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+        testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
     }
 
     tasks.withType<KotlinCompile> {
@@ -52,5 +59,33 @@ subprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+
+    val asciidoctorExt: Configuration by configurations.creating
+    dependencies {
+        asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+    }
+
+    val snippetsDir by extra { file("build/generated-snippets") }
+    tasks {
+        test {
+            outputs.dir(snippetsDir)
+        }
+
+        asciidoctor {
+            inputs.dir(snippetsDir)
+            configurations(asciidoctorExt.name)
+            dependsOn(test)
+            doLast {
+                copy {
+                    from("build/docs/asciidoc")
+                    into("src/main/resources/static/docs")
+                }
+            }
+        }
+
+        build {
+            dependsOn(asciidoctor)
+        }
     }
 }
