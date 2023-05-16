78377b3cb6575bf6bbecdfba4c66ee92de1279bc
rara2 <kma952727@gmail.com>
2023-05-17 08:57:34 +0900

    refactor : RegisterDto Email 타입 검증

--- perfume-api/src/main/java/io/perfume/api/user/infrastructure/api/dto/RegisterDto.java
+++ perfume-api/src/main/java/io/perfume/api/user/infrastructure/api/dto/RegisterDto.java
@@ -1,8 +1,9 @@
 package io.perfume.api.user.infrastructure.api.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RegisterDto(@NotBlank String username, @NotBlank String password, @NotBlank String email,
+public record RegisterDto(@NotBlank String username, @NotBlank String password, @Email String email,
                           @NotNull boolean marketingConsent, @NotNull boolean promotionConsent, @NotNull String name) {
 }
