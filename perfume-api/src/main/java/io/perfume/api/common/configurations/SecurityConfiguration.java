6291409f17dce97e5e7a033186bdd7b2463910ed
dygma0 <404err@naver.com>
2023-05-26 20:00:11 +0900

    chore: oauth2 configuration

---
+++ perfume-api/src/main/java/io/perfume/api/common/configurations/SecurityConfiguration.java
@@ -0,0 +1,64 @@
+package io.perfume.api.common.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity httpSecurity,
+            AuthorizationRequestRepository<OAuth2AuthorizationRequest> requestAuthorizationRequestRepository,
+            WhiteListConfiguration whiteListConfig
+    ) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        authorizeHttpRequests ->
+                                authorizeHttpRequests
+                                        .anyRequest().permitAll()
+                )
+                .csrf(CsrfConfigurer::disable)
+                .cors(c -> c.configurationSource(corsConfigurationSource(whiteListConfig.getCors())))
+                .oauth2Login(oAuth2LoginConfigurer ->
+                            oAuth2LoginConfigurer
+                                    .authorizationEndpoint(authorizationEndpointConfig ->
+                                            authorizationEndpointConfig
+                                                    .baseUri("/oauth2/authorize")
+                                                    .authorizationRequestRepository(requestAuthorizationRequestRepository)));
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(List<String> whitelistCors) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(whitelistCors);
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> httpSessionOAuth2AuthorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
+    }
+}
