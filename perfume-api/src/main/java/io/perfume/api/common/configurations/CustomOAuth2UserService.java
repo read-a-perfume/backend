ab8387c921a64cb1263b747fde1061177ff447f3
dygma0 <404err@naver.com>
2023-05-27 23:33:57 +0900

    feat: CustomOAuth2UserService 추가

---
+++ perfume-api/src/main/java/io/perfume/api/common/configurations/CustomOAuth2UserService.java
@@ -0,0 +1,19 @@
+package io.perfume.api.common.configurations;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final DefaultOAuth2UserService oAuth2UserService = new DefaultOAuth2UserService();
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        return oAuth2UserService.loadUser(userRequest);
+    }
+}
