fe597ed0efd1cf9824178adb5f9613f868e0abb7
dygma0 <404err@naver.com>
2023-05-27 23:34:57 +0900

    feat: OAuth2 설정 수정

--- perfume-api/src/main/java/io/perfume/api/common/configurations/SecurityConfiguration.java
+++ perfume-api/src/main/java/io/perfume/api/common/configurations/SecurityConfiguration.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -23,6 +26,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(
             HttpSecurity httpSecurity,
             AuthorizationRequestRepository<OAuth2AuthorizationRequest> requestAuthorizationRequestRepository,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService,
             WhiteListConfiguration whiteListConfig
     ) throws Exception {
         httpSecurity
@@ -34,11 +38,19 @@ public class SecurityConfiguration {
                 .csrf(CsrfConfigurer::disable)
                 .cors(c -> c.configurationSource(corsConfigurationSource(whiteListConfig.getCors())))
                 .oauth2Login(oAuth2LoginConfigurer ->
-                            oAuth2LoginConfigurer
-                                    .authorizationEndpoint(authorizationEndpointConfig ->
-                                            authorizationEndpointConfig
-                                                    .baseUri("/oauth2/authorize")
-                                                    .authorizationRequestRepository(requestAuthorizationRequestRepository)));
+                        oAuth2LoginConfigurer
+                                .authorizationEndpoint(authorizationEndpointConfig ->
+                                        authorizationEndpointConfig
+                                                .baseUri("/oauth2/authorize")
+                                                .authorizationRequestRepository(requestAuthorizationRequestRepository))
+                                .redirectionEndpoint(redirectionEndpointConfig ->
+                                        redirectionEndpointConfig
+                                                .baseUri("/login/oauth2/code/*"))
+                                .userInfoEndpoint(userInfoEndpointConfig ->
+                                        userInfoEndpointConfig
+                                                .userService(oAuth2UserService)
+                                )
+                );
 
         return httpSecurity.build();
     }
