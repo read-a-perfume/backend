9162d0fc747d9339abee7491de5c968a5bf4e346
Big-Cir97 <eodnjs01477@gmail.com>
2023-05-30 10:13:13 +0900

    test: /login API 테스트 추가

--- perfume-api/src/main/java/io/perfume/api/common/filters/LoginAuthenticationFilter.java
+++ perfume-api/src/main/java/io/perfume/api/common/filters/LoginAuthenticationFilter.java
@@ -15,10 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 
 import java.io.IOException;
 
---
+++ perfume-api/src/test/java/io/perfume/api/common/filters/LoginAuthenticationFilterTest.java
@@ -0,0 +1,67 @@
+package io.perfume.api.common.filters;
+
+import io.perfume.api.common.configurations.SecurityConfiguration;
+import io.perfume.api.user.adapter.in.http.dto.RegisterDto;
+import io.perfume.api.user.application.service.RegisterService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Import(SecurityConfiguration.class)
+@AutoConfigureMockMvc
+@Transactional
+class LoginAuthenticationFilterTest {
+
+    @Autowired
+    RegisterService registerService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        RegisterDto register = new RegisterDto("test@test.com", passwordEncoder.encode("test12341234"), "test@gmail.com",
+                false, false, "testUser");
+
+        registerService.signUpGeneralUserByEmail(register);
+    }
+
+    @Test
+    @DisplayName("로그인 성공 시 jwt 토큰을 생성한다.")
+    void successLoginGenerateToken () throws Exception {
+        MvcResult mvcResult = this.mockMvc.perform(post("/login")
+                        .contentType(MediaType.TEXT_HTML)
+                        .param("username", "test@test.com")
+                        .param("password", "test12341234"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        
+        String token = mvcResult.getResponse().getHeader("Authorization");
+
+        assertNotNull(token);
+        assertEquals(token.substring(0, 7), "Bearer ");
+    }
+
+}
