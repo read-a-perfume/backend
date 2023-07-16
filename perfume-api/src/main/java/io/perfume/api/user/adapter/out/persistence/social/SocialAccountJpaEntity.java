94f6d3034976badc02e2937a2c8be711763a7098
dygma0 <404err@naver.com>
2023-07-16 18:42:01 +0900

    [RDPF-126] Social account user FK 제거

--- perfume-api/src/main/java/io/perfume/api/user/adapter/out/persistence/social/SocialAccountJpaEntity.java
+++ perfume-api/src/main/java/io/perfume/api/user/adapter/out/persistence/social/SocialAccountJpaEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,8 +26,9 @@ import lombok.ToString;
 
 @Entity(name = "social_account")
 @Table(
+    name = "social_account",
     uniqueConstraints = {
-        @UniqueConstraint(name = "uni_identifier", columnNames = "identifier")
+        @UniqueConstraint(name = "uni_social_account_1", columnNames = "identifier")
     }
 )
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
@@ -54,8 +56,12 @@ public class SocialAccountJpaEntity extends BaseTimeEntity {
   @Enumerated(EnumType.STRING)
   SocialProvider socialProvider;
 
-  @ManyToOne(cascade = CascadeType.PERSIST)
-  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  @ManyToOne
+  @JoinColumn(
+      name = "user_id",
+      referencedColumnName = "id",
+      foreignKey = @ForeignKey(name = "none")
+  )
   UserJpaEntity user;
 
   public SocialAccountJpaEntity(Long id, String identifier, String email,
