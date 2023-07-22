b5e40b45d2c99be4d425b4c74089b8f39ba01c1e
dygma0 <404err@naver.com>
2023-07-22 10:34:46 +0900

    [RDPF-100] note-user 연관 관계 entity 추가

--- perfume-api/src/main/java/io/perfume/api/note/adapter/out/persistence/NoteUserJpaEntity.java
+++ perfume-api/src/main/java/io/perfume/api/note/adapter/out/persistence/NoteUserJpaEntity.java
@@ -1,2 +1,24 @@
-package io.perfume.api.note.adapter.out.persistence;public class NoteUserJpaEntity {
+package io.perfume.api.note.adapter.out.persistence;
+
+import io.perfume.api.base.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+public class NoteUserJpaEntity extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @EqualsAndHashCode.Include
+  @ToString.Include
+  private Long id;
+
+  @Column(nullable = false)
+  private Long noteId;
+
+  @Column(nullable = false)
+  private Long userId;
 }
