4e56af25436d894df40d5992b457a28e6eb857f8
rara2 <kma952727@gmail.com>
2023-07-22 22:10:56 +0900

    [RDPF-63] FileMapper class @Componenet 부착

--- perfume-api/src/main/java/io/perfume/api/file/adapter/out/persistence/FileMapper.java
+++ perfume-api/src/main/java/io/perfume/api/file/adapter/out/persistence/FileMapper.java
@@ -1,7 +1,9 @@
 package io.perfume.api.file.adapter.out.persistence;
 
 import io.perfume.api.file.domain.File;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FileMapper {
     public File toDomain(FileJpaEntity entity) {
         if(entity == null) {
