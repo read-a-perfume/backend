d078ca7dafbcc68cb16322123f8b31736e53cdee
dygma0 <404err@naver.com>
2023-07-30 23:10:52 +0900

    [RDPF-172] JWT filter 수정

--- perfume-api/src/main/java/io/perfume/api/common/jwt/JwtAuthenticationFilter.java
+++ perfume-api/src/main/java/io/perfume/api/common/jwt/JwtAuthenticationFilter.java
@@ -2,14 +2,13 @@ package io.perfume.api.common.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Objects;
-import jwt.JsonWebTokenGenerator;
+import java.util.Arrays;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,12 +18,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final AuthenticationManager authenticationManager;
-  private final JsonWebTokenGenerator jsonWebTokenGenerator;
 
-  public JwtAuthenticationFilter(AuthenticationManager authenticationManager,
-                                 JsonWebTokenGenerator jsonWebTokenGenerator) {
+  public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
     this.authenticationManager = authenticationManager;
-    this.jsonWebTokenGenerator = jsonWebTokenGenerator;
   }
 
   @Override
@@ -32,16 +28,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                   @NotNull HttpServletResponse response,
                                   @NotNull FilterChain filterChain)
       throws ServletException, IOException {
-    String jwt = jsonWebTokenGenerator.getTokenFromCookie(request);
+    Arrays.stream(request.getCookies())
+        .filter(cookie -> cookie.getName().equalsIgnoreCase("x-access-token"))
+        .findFirst()
+        .map(Cookie::getValue)
+        .ifPresent(value -> {
+          var authentication =
+              authenticationManager.authenticate(new JwtAuthenticationToken(value));
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+        });
 
-    if (Objects.isNull(jwt)) {
-      filterChain.doFilter(request, response);
-      return;
-    }
-
-    Authentication authentication =
-        authenticationManager.authenticate(new JwtAuthenticationToken(jwt));
-    SecurityContextHolder.getContext().setAuthentication(authentication);
     filterChain.doFilter(request, response);
   }
 }
