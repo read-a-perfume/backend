14460be33f1399ab6ccdb00d1e108245fe0cf7c3
dygma0 <404err@naver.com>
2023-07-30 23:30:14 +0900

    [RDPF-172] 토큰 재발행시 쿠키 설정

--- perfume-api/src/main/java/io/perfume/api/auth/adapter/in/http/TokenHandlingController.java
+++ perfume-api/src/main/java/io/perfume/api/auth/adapter/in/http/TokenHandlingController.java
@@ -2,34 +2,42 @@ package io.perfume.api.auth.adapter.in.http;
 
 import io.perfume.api.auth.application.port.in.MakeNewTokenUseCase;
 import io.perfume.api.auth.application.port.in.dto.ReissuedTokenResult;
+import io.perfume.api.common.auth.Constants;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
 public class TokenHandlingController {
+
   private final MakeNewTokenUseCase makeNewTokenUseCase;
 
   @GetMapping("/v1/reissue")
   public ResponseEntity<Void> reissueAccessToken(
-      @CookieValue(name = "X-Refresh-Token") String refreshToken,
-      @RequestHeader(name = "Authorization") String accessToken) {
-    ReissuedTokenResult reissuedTokenResult =
+      @CookieValue(name = Constants.ACCESS_TOKEN_KEY) String accessToken,
+      @CookieValue(name = Constants.REFRESH_TOKEN_KEY) String refreshToken) {
+    var reissuedTokenResult =
         makeNewTokenUseCase.reissueAccessToken(accessToken, refreshToken, LocalDateTime.now());
-    String cookie = ResponseCookie.from("X-Refresh-Token", reissuedTokenResult.refreshToken())
-        .httpOnly(true)
-        .secure(true).build().toString();
-    return ResponseEntity.ok()
-        .header("Authorization", reissuedTokenResult.accessToken())
-        .header(HttpHeaders.SET_COOKIE, cookie).build();
+
+    return ResponseEntity
+        .ok()
+        .headers(createAuthenticationCookie(reissuedTokenResult))
+        .build();
+  }
+
+  private HttpHeaders createAuthenticationCookie(ReissuedTokenResult reissuedTokenResult) {
+    var responseHeaders = new HttpHeaders();
+
+    responseHeaders.add(Constants.ACCESS_TOKEN_KEY, reissuedTokenResult.accessToken());
+    responseHeaders.add(Constants.REFRESH_TOKEN_KEY, reissuedTokenResult.refreshToken());
+
+    return responseHeaders;
   }
 }
