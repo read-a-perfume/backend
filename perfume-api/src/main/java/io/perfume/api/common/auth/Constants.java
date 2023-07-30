e481c4209c14d68e73d1ff4be2dfe0f18edfb607
dygma0 <404err@naver.com>
2023-07-30 23:30:14 +0900

    [RDPF-172] 토큰 재발행시 쿠키 설정

---
+++ perfume-api/src/main/java/io/perfume/api/common/auth/Constants.java
@@ -0,0 +1,10 @@
+package io.perfume.api.common.auth;
+
+public final class Constants {
+
+  public static final String ACCESS_TOKEN_KEY = "X-Access-Token";
+
+  public static final String REFRESH_TOKEN_KEY = "X-Refresh-Token";
+
+  public static final String USER_ID_KEY = "userId";
+}
