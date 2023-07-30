79135c910fbb3abd19b87bad35be1d5d8e7f457d
dygma0 <404err@naver.com>
2023-07-31 00:07:47 +0900

    [RDPF-172] 소셜 회원가입 리팩토링

--- perfume-api/src/main/java/io/perfume/api/user/application/port/in/dto/SignUpSocialUserCommand.java
+++ perfume-api/src/main/java/io/perfume/api/user/application/port/in/dto/SignUpSocialUserCommand.java
@@ -1,6 +1,9 @@
 package io.perfume.api.user.application.port.in.dto;
 
 import io.perfume.api.user.domain.SocialProvider;
+import java.time.Instant;
+import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
 
 public record SignUpSocialUserCommand(
     String identifier,
@@ -10,4 +13,24 @@ public record SignUpSocialUserCommand(
     String name,
     SocialProvider socialProvider
 ) {
+
+  public static SignUpSocialUserCommand byGoogle(String identifier, String email,
+                                          String password, String name) {
+    return new SignUpSocialUserCommand(identifier, email, generateRandomUsername(email), password,
+        name, SocialProvider.GOOGLE);
+  }
+
+  @NotNull
+  private static Long getUnixTime() {
+    return Instant.now().getEpochSecond();
+  }
+
+  @NotNull
+  private static String generateRandomUsername(String email) {
+    if (Objects.isNull(email) || !email.contains("@")) {
+      throw new IllegalArgumentException(email + " 올바른 이메일 형식이 아닙니다.");
+    }
+
+    return email.split("@")[0] + getUnixTime();
+  }
 }
