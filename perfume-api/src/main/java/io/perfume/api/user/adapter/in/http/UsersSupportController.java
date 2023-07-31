359d19c7921ba437df157e3ea3dadaed97807891
rara2 <kma952727@gmail.com>
2023-07-31 14:38:03 +0900

    [RDPF-64] @AuthenticationPrincipal 참조 클래스 변경
    1.  io.perfume.api.user.domain.User -> org.springframework.security.core.userdetails.User

--- perfume-api/src/main/java/io/perfume/api/user/adapter/in/http/UsersSupportController.java
+++ perfume-api/src/main/java/io/perfume/api/user/adapter/in/http/UsersSupportController.java
@@ -4,13 +4,13 @@ import io.perfume.api.common.auth.UserPrincipal;
 import io.perfume.api.user.application.port.in.FindEncryptedUsernameUseCase;
 import io.perfume.api.user.application.port.in.LeaveUserUseCase;
 import io.perfume.api.user.application.port.in.SendResetPasswordMailUseCase;
-import io.perfume.api.user.domain.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
