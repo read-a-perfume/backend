32d2b14dff127c646777c6f756317f0c0e30bc9c
dygma0 <404err@naver.com>
2023-09-10 15:41:06 +0900

    [RDPF-187] Find user by user ids

--- perfume-api/src/main/java/io/perfume/api/user/adapter/out/persistence/user/UserQueryPersistenceAdapter.java
+++ perfume-api/src/main/java/io/perfume/api/user/adapter/out/persistence/user/UserQueryPersistenceAdapter.java
@@ -1,10 +1,12 @@
 package io.perfume.api.user.adapter.out.persistence.user;
 
+import static io.perfume.api.user.adapter.out.persistence.user.QUserEntity.userEntity;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import io.perfume.api.base.PersistenceAdapter;
 import io.perfume.api.user.application.port.out.UserQueryRepository;
 import io.perfume.api.user.domain.User;
-import java.util.Objects;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
@@ -18,58 +20,71 @@ public class UserQueryPersistenceAdapter implements UserQueryRepository {
 
   @Override
   public Optional<User> findOneByEmail(String email) {
-    UserEntity userEntity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
+    UserEntity entity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
         .where(
             QUserEntity.userEntity.email.eq(email)
                 .and(QUserEntity.userEntity.deletedAt.isNull()))
         .fetchOne();
-    return Optional.ofNullable(userMapper.toUser(userEntity));
+
+    return Optional.ofNullable(userMapper.toUser(entity));
   }
 
   @Override
   public Optional<User> loadUser(long userId) {
-    UserEntity userEntity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
+    UserEntity entity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
         .where(
             QUserEntity.userEntity.id.eq(userId)
                 .and(QUserEntity.userEntity.deletedAt.isNull()))
         .fetchOne();
-    return Optional.ofNullable(userMapper.toUser(userEntity));
+
+    return Optional.ofNullable(userMapper.toUser(entity));
   }
 
   @Override
   public Optional<User> findByUsername(String username) {
-    UserEntity userEntity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
+    UserEntity entity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
         .where(
             QUserEntity.userEntity.username.eq(username)
                 .and(QUserEntity.userEntity.deletedAt.isNull()))
         .fetchOne();
-    return Optional.ofNullable(userMapper.toUser(userEntity));
+
+    return Optional.ofNullable(userMapper.toUser(entity));
   }
 
   @Override
   public Optional<User> findOneByEmailAndUsername(String email, String username) {
-    UserEntity userEntity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
+    UserEntity entity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
         .where(
             QUserEntity.userEntity.username.eq(username)
                 .and(QUserEntity.userEntity.email.eq(email))
                 .and(QUserEntity.userEntity.deletedAt.isNull()))
         .fetchOne();
-    return Optional.ofNullable(userMapper.toUser(userEntity));
+
+    return Optional.ofNullable(userMapper.toUser(entity));
   }
 
   @Override
   public Optional<User> findOneByEmailOrUsername(String emailOrUsername) {
-    UserEntity userEntity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
+    UserEntity entity = jpaQueryFactory.selectFrom(QUserEntity.userEntity)
         .where(
             QUserEntity.userEntity.username.eq(emailOrUsername)
                 .or(QUserEntity.userEntity.email.eq(emailOrUsername))
                 .and(QUserEntity.userEntity.deletedAt.isNull()))
         .fetchOne();
 
-    if (Objects.isNull(userEntity)) {
-      return Optional.empty();
-    }
+    return Optional.ofNullable(userMapper.toUser(entity));
+  }
 
-    return Optional.of(userMapper.toUser(userEntity));
+  @Override
+  public List<User> findUsersByIds(List<Long> userIds) {
+    return jpaQueryFactory
+        .selectFrom(userEntity)
+        .where(
+            userEntity.id.in(userIds)
+                .and(userEntity.deletedAt.isNull()))
+        .fetch()
+        .stream()
+        .map(userMapper::toUser)
+        .toList();
   }
 }
--- perfume-api/src/main/java/io/perfume/api/user/application/port/in/FindUserUseCase.java
+++ perfume-api/src/main/java/io/perfume/api/user/application/port/in/FindUserUseCase.java
@@ -1,6 +1,7 @@
 package io.perfume.api.user.application.port.in;
 
 import io.perfume.api.user.application.port.in.dto.UserResult;
+import java.util.List;
 import java.util.Optional;
 
 public interface FindUserUseCase {
@@ -9,4 +10,5 @@ public interface FindUserUseCase {
 
   Optional<UserResult> findOneBySocialId(String socialId);
 
+  List<UserResult> findUsersByIds(List<Long> userIds);
 }
