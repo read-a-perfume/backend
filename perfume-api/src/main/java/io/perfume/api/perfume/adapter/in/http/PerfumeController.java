4db6d6ce9c0e48043a6076b29cc4e83296688895
dygma0 <404err@naver.com>
2023-09-17 21:53:18 +0900

    [RDPF-171] CreatePerfume API 리팩토링
    Co-authored-by: oliviarla <imoliviarla@gmail.com>
    Co-authored-by: Big-Cir97 <eodnjs01477@gmail.com>

--- perfume-api/src/main/java/io/perfume/api/perfume/adapter/in/http/PerfumeController.java
+++ perfume-api/src/main/java/io/perfume/api/perfume/adapter/in/http/PerfumeController.java
@@ -6,7 +6,12 @@ import io.perfume.api.perfume.application.port.in.CreatePerfumeUseCase;
 import io.perfume.api.perfume.application.port.in.GetPerfumeUseCase;
 import io.perfume.api.perfume.application.port.in.dto.CreatePerfumeCommand;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/perfumes")
@@ -14,26 +19,18 @@ import org.springframework.web.bind.annotation.*;
 public class PerfumeController {
 
   private final GetPerfumeUseCase getPerfumeUseCase;
+
   private final CreatePerfumeUseCase createPerfumeUseCase;
 
   @GetMapping("/{id}")
   public PerfumeResponse findPerfumeById(@PathVariable Long id) {
-        return PerfumeResponse.of(getPerfumeUseCase.getPerfume(id));
+    return PerfumeResponse.of(getPerfumeUseCase.getPerfume(id));
   }
 
   @PostMapping
   public void createPerfume(@RequestBody CreatePerfumeRequestDto createPerfumeRequestDto) {
-    CreatePerfumeCommand createPerfumeCommand = new CreatePerfumeCommand(createPerfumeRequestDto.name(),
-        createPerfumeRequestDto.story(),
-        createPerfumeRequestDto.concentration(),
-        createPerfumeRequestDto.price(),
-        createPerfumeRequestDto.capacity(),
-        createPerfumeRequestDto.brandId(),
-        createPerfumeRequestDto.categoryId(),
-        createPerfumeRequestDto.thumbnailId(),
-        createPerfumeRequestDto.topNoteIds(),
-        createPerfumeRequestDto.middleNoteIds(),
-        createPerfumeRequestDto.baseNoteIds());
+    CreatePerfumeCommand createPerfumeCommand = createPerfumeRequestDto.toCommand();
+
     createPerfumeUseCase.createPerfume(createPerfumeCommand);
   }
 }
