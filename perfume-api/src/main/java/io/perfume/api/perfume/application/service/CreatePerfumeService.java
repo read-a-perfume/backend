f13dd81781d98f86b6bff016a942aec728732972
dygma0 <404err@naver.com>
2023-09-17 21:54:46 +0900

    [RDPF-171] createPerfume refactoring
    Co-authored-by: oliviarla <imoliviarla@gmail.com>
    Co-authored-by: Big-Cir97 <eodnjs01477@gmail.com>

--- perfume-api/src/main/java/io/perfume/api/perfume/application/service/CreatePerfumeService.java
+++ perfume-api/src/main/java/io/perfume/api/perfume/application/service/CreatePerfumeService.java
@@ -3,7 +3,7 @@ package io.perfume.api.perfume.application.service;
 import io.perfume.api.perfume.adapter.out.persistence.perfume.PerfumePersistenceAdapter;
 import io.perfume.api.perfume.application.port.in.CreatePerfumeUseCase;
 import io.perfume.api.perfume.application.port.in.dto.CreatePerfumeCommand;
-import io.perfume.api.perfume.domain.NotePyramid;
+import io.perfume.api.perfume.domain.NotePyramidIds;
 import io.perfume.api.perfume.domain.Perfume;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +11,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CreatePerfumeService implements CreatePerfumeUseCase {
+
   private final PerfumePersistenceAdapter perfumePersistenceAdapter;
 
   @Override
   public void createPerfume(CreatePerfumeCommand createPerfumeCommand) {
+    NotePyramidIds notePyramidIds = NotePyramidIds.builder()
+        .topNoteIds(createPerfumeCommand.topNoteIds())
+        .middleNoteIds(createPerfumeCommand.middleNoteIds())
+        .baseNoteIds(createPerfumeCommand.baseNoteIds())
+        .build();
+
     Perfume perfume = Perfume.builder()
         .name(createPerfumeCommand.name())
         .story(createPerfumeCommand.story())
@@ -24,12 +31,8 @@ public class CreatePerfumeService implements CreatePerfumeUseCase {
         .brandId(createPerfumeCommand.brandId())
         .categoryId(createPerfumeCommand.categoryId())
         .thumbnailId(createPerfumeCommand.thumbnailId())
+        .notePyramidIds(notePyramidIds)
         .build();
-//    NotePyramid notePyramid = NotePyramid.builder()
-//        .topNoteIds(createPerfumeCommand.topNoteIds())
-//        .middleNoteIds(createPerfumeCommand.middleNoteIds())
-//        .baseNoteIds(createPerfumeCommand.baseNoteIds())
-//        .build();
 
     perfumePersistenceAdapter.save(perfume);
   }
