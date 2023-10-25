43098cfa4bba309ed82aff71fa8634533afec67f
Big-Cir97 <eodnjs01477@gmail.com>
2023-10-25 19:44:43 +0900

    [RDPF-193] feat: Follow Service 추가

--- perfume-api/src/main/java/io/perfume/api/perfume/adapter/in/http/PerfumeController.java
+++ perfume-api/src/main/java/io/perfume/api/perfume/adapter/in/http/PerfumeController.java
@@ -11,14 +11,11 @@ import io.perfume.api.perfume.application.port.in.dto.SimplePerfumeResult;
 import jakarta.annotation.Nullable;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-=======
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
->>>>>>> 9e993a3 (FEAT: 브랜드 별 향수 목록 조회 기능 추가)
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,19 +50,17 @@ public class PerfumeController {
     createPerfumeUseCase.createPerfume(createPerfumeCommand);
   }
 
-<<<<<<< HEAD
   @PreAuthorize("isAuthenticated()")
   @PostMapping("/favorite/{id}")
   public void favoritePerfume(@AuthenticationPrincipal User user, @PathVariable Long id) {
     var userId = Long.parseLong(user.getUsername());
     userFavoritePerfumeUseCase.addAndDeleteFavoritePerfume(userId, id);
-=======
+
   @GetMapping
   public Slice<SimplePerfumeResponseDto> getPerfumesByBrand(@RequestParam Long brandId, @RequestParam @Nullable Long lastPerfumeId,
                                                             @RequestParam int pageSize) {
     Slice<SimplePerfumeResult> perfumesByBrand = findPerfumeUseCase.findPerfumesByBrand(brandId, lastPerfumeId, pageSize);
     List<SimplePerfumeResponseDto> list = perfumesByBrand.getContent().stream().map(SimplePerfumeResponseDto::of).toList();
     return new SliceImpl<>(list, perfumesByBrand.getPageable(), perfumesByBrand.hasNext());
->>>>>>> 9e993a3 (FEAT: 브랜드 별 향수 목록 조회 기능 추가)
   }
 }
