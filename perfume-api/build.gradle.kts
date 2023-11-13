4a7a254219e0f60e1372d3b61971271459c3e154
dygma0 <404err@naver.com>
2023-11-13 13:34:49 +0900

    Revert "fix(deps): update dependency org.flywaydb:flyway-mysql to v10"
    This reverts commit bd2040af38bf40168b45f5dc1055572ef054123d.

--- perfume-api/build.gradle.kts
+++ perfume-api/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("mysql:mysql-connector-java:8.0.33")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
-    implementation("org.flywaydb:flyway-mysql:10.0.0")
+    implementation("org.flywaydb:flyway-mysql:9.22.3")
 
     runtimeOnly("org.springframework.boot:spring-boot-devtools:3.1.5")
     compileOnly("org.projectlombok:lombok:1.18.30")
