618488e12d7cf96d1937ddf9c68d7c2695648b8d
oliviarla <imoliviarla@gmail.com>
2023-11-25 13:14:10 +0900

    feat: access token 만료 시 에러 처리

---
+++ perfume-api/src/main/java/io/perfume/api/common/jwt/TokenExpiredException.java
@@ -0,0 +1,10 @@
+package io.perfume.api.common.jwt;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+public class TokenExpiredException extends AuthenticationException {
+  public TokenExpiredException() {
+    super("Access Token Expired.");
+  }
+}
