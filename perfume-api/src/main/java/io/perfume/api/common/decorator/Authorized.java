e88b0cb7748c66b00033f5c11dc3d2ce6576b5ad
dygma0 <404err@naver.com>
2023-11-30 15:43:22 +0900

    feat: 인가 정책 수정

---
+++ perfume-api/src/main/java/io/perfume/api/common/decorator/Authorized.java
@@ -0,0 +1,13 @@
+package io.perfume.api.common.decorator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("isAuthenticated()")
+public @interface Authorized {
+}
