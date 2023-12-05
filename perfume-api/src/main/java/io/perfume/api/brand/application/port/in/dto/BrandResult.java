9daa6f9acfc22c505e7b851b67a9cab79201ab3b
oliviarla <imoliviarla@gmail.com>
2023-12-05 23:39:40 +0900

    fix: thumbnailUrl -> thumbnail로 통일

--- perfume-api/src/main/java/io/perfume/api/brand/application/port/in/dto/BrandResult.java
+++ perfume-api/src/main/java/io/perfume/api/brand/application/port/in/dto/BrandResult.java
@@ -3,7 +3,7 @@ package io.perfume.api.brand.application.port.in.dto;
 import io.perfume.api.brand.domain.Brand;
 import io.perfume.api.file.domain.File;
 
-public record BrandResult(String name, String story, String thumbnailUrl) {
+public record BrandResult(String name, String story, String thumbnail) {
   public static BrandResult of(Brand brand, File file) {
     return new BrandResult(brand.getName(), brand.getStory(), file.getUrl());
   }
