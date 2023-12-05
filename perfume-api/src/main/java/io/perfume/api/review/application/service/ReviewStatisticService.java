399cd62f8fb0a453a1cfadd9978725b662ceddb2
oliviarla <imoliviarla@gmail.com>
2023-12-06 00:08:46 +0900

    fix: 향수 데이터 통계 시 리뷰 없을 때 버그 수정

--- perfume-api/src/main/java/io/perfume/api/review/application/service/ReviewStatisticService.java
+++ perfume-api/src/main/java/io/perfume/api/review/application/service/ReviewStatisticService.java
@@ -4,7 +4,13 @@ import io.perfume.api.review.adapter.out.persistence.repository.ReviewQueryPersi
 import io.perfume.api.review.application.in.ReviewStatisticUseCase;
 import io.perfume.api.review.application.in.dto.ReviewStatisticResult;
 import io.perfume.api.review.domain.ReviewFeatureCount;
+import io.perfume.api.review.domain.type.DayType;
+import io.perfume.api.review.domain.type.Duration;
+import io.perfume.api.review.domain.type.Season;
+import io.perfume.api.review.domain.type.Strength;
+import io.perfume.api.user.adapter.out.persistence.user.Sex;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +29,15 @@ public class ReviewStatisticService implements ReviewStatisticUseCase {
     ReviewFeatureCount reviewFeatureCount = reviewQueryPersistenceAdapter.getReviewFeatureCount(perfumeId);
 
     Long totalReviews = reviewFeatureCount.totalReviews();
+    if (totalReviews == 0) {
+      return new ReviewStatisticResult(
+          createEmptyMap(Strength.class),
+          createEmptyMap(Duration.class),
+          createEmptyMap(Season.class),
+          createEmptyMap(DayType.class),
+          createEmptyMap(Sex.class)
+      );
+    }
     return new ReviewStatisticResult(
         calculatePercentage(reviewFeatureCount.strengthMap(), totalReviews),
         calculatePercentage(reviewFeatureCount.durationMap(), totalReviews),
@@ -32,12 +47,20 @@ public class ReviewStatisticService implements ReviewStatisticUseCase {
     );
   }
 
-  private static <V extends Enum<V>> Map<V, Long> calculatePercentage(Map<V, Long> map, Long totalReviews) {
+  private <V extends Enum<V>> Map<V, Long> calculatePercentage(Map<V, Long> map, Long totalReviews) {
     EnumSet.allOf(map.keySet().iterator().next().getDeclaringClass()).forEach(
         enumValue -> map.putIfAbsent(enumValue, 0L)
     );
 
     return map.entrySet().stream()
         .collect(Collectors.toMap(Map.Entry::getKey, e -> Math.round((double) e.getValue() / totalReviews * 100)));
+  }
+
+  private <V extends Enum<V>> Map<V, Long> createEmptyMap(Class<V> enumClass) {
+    Map<V, Long> map = new HashMap<>();
+    EnumSet.allOf(enumClass).forEach(
+        enumValue -> map.putIfAbsent(enumValue, 0L)
+    );
+    return map;
   }
 }
