03b3cec3691aeeddee6b823decc836a2178c8de6
Big-Cir97 <eodnjs01477@gmail.com>
2023-12-06 11:52:58 +0900

    feat: flyway script, restdocs 추가

--- perfume-api/src/test/java/io/perfume/api/mypage/adapter/port/out/persistence/follow/FollowQueryPersistenceAdapterTest.java
+++ perfume-api/src/test/java/io/perfume/api/mypage/adapter/port/out/persistence/follow/FollowQueryPersistenceAdapterTest.java
@@ -10,6 +10,8 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -31,8 +33,8 @@ class FollowQueryPersistenceAdapterTest {
   @Test
   void testFindByFollowerId() {
     // given
-    var followerId = 0L;
-    var followingId = 1L;
+    var followerId = 1L;
+    var followingId = 2L;
     var now = LocalDateTime.now();
     var follow = Follow.create(
         followerId,
@@ -49,9 +51,8 @@ class FollowQueryPersistenceAdapterTest {
     var find = queryRepository.findByFollowerId(followerId).orElseThrow();
 
     // then
-    assertThat(find.getId()).isEqualTo(1L);
-    assertThat(find.getFollowerId()).isEqualTo(0L);
-    assertThat(find.getFollowingId()).isEqualTo(1L);
+    assertThat(find.getFollowerId()).isEqualTo(1L);
+    assertThat(find.getFollowingId()).isEqualTo(2L);
     assertThat(find.getDeletedAt()).isNull();
   }
 
@@ -59,8 +60,8 @@ class FollowQueryPersistenceAdapterTest {
   @DisplayName("팔로워와 팔로잉 id로 조회")
   void testFindByFollowerIdAndFollowingId() {
     // given
-    var followerId = 0L;
-    var followingId = 1L;
+    var followerId = 1L;
+    var followingId = 2L;
     var now = LocalDateTime.now();
     var follow = Follow.create(
         followerId,
@@ -78,68 +79,62 @@ class FollowQueryPersistenceAdapterTest {
         .findByFollowerIdAndFollowingId(followerId, followingId).orElseThrow();
 
     // then
-    assertThat(find.getId()).isEqualTo(1L);
+    assertThat(find.getId()).isEqualTo(2L);
   }
 
   @Test
-  @DisplayName("사용자 팔로잉  확인")
+  @DisplayName("사용자 팔로잉 확인")
   void testFindFollowingCountByFollowerId() {
-    long followCount = 10;
+    long followCount = 1;
 
     // given
-    var followerId = 0L;
-    var followingId = 1L;
+    var followerId = 1L;
+    var followingId = 2L;
     var now = LocalDateTime.now();
-    for (int i = 0; i < followCount; i++) {
-      var follow = Follow.create(
-          followerId,
-          followingId,
-          now
-      );
-      followingId++;
+    var follow = Follow.create(
+        followerId,
+        followingId,
+        now
+    );
 
-      var entity = followMapper.toEntity(follow);
-      entityManager.persist(entity);
-    }
+    var entity = followMapper.toEntity(follow);
+    entityManager.persist(entity);
     entityManager.flush();
     entityManager.clear();
 
     // when
-    Long find = queryRepository.findFollowingCountByFollowerId(0L);
+    Long find = queryRepository.findFollowingCountByFollowerId(1L);
 
     // then
-    assertThat(followCount).isEqualTo(10L);
+    assertThat(followCount).isEqualTo(1L);
     assertThat(followCount).isEqualTo(find);
   }
 
   @Test
   @DisplayName("사용자를 팔로우 확인")
   void testFindFollowerCountByFollowingId() {
-    long followCount = 10;
+    long followCount = 1;
 
     // given
-    var followerId = 0L;
-    var followingId = 1L;
+    var followerId = 1L;
+    var followingId = 2L;
     var now = LocalDateTime.now();
-    for (int i = 0; i < followCount; i++) {
-      var follow = Follow.create(
-          followerId,
-          followingId,
-          now
-      );
-      followerId++;
+    var follow = Follow.create(
+        followerId,
+        followingId,
+        now
+    );
 
-      var entity = followMapper.toEntity(follow);
-      entityManager.persist(entity);
-    }
+    var entity = followMapper.toEntity(follow);
+    entityManager.persist(entity);
     entityManager.flush();
     entityManager.clear();
 
     // when
-    Long find = queryRepository.findFollowerCountByFollowingId(1L);
+    Long find = queryRepository.findFollowerCountByFollowingId(2L);
 
     // then
-    assertThat(followCount).isEqualTo(10L);
+    assertThat(followCount).isEqualTo(1L);
     assertThat(followCount).isEqualTo(find);
   }
 }
