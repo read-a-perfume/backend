2983d916f7e519b521dcc71fb0cedcf10d1a1b9d
Byeongju, Shin <404err@naver.com>
2023-12-12 19:03:09 +0900

    feat: 임시 파일 업로드 API 구현

---
+++ perfume-api/src/main/java/io/perfume/api/file/adapter/in/http/FileController.java
@@ -0,0 +1,39 @@
+package io.perfume.api.file.adapter.in.http;
+
+import io.perfume.api.file.adapter.in.http.dto.UpdateFileResponseDto;
+import io.perfume.api.file.application.port.in.dto.FileResult;
+import io.perfume.api.file.application.service.FileService;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/files")
+@PreAuthorize("isAuthenticated()")
+@RequiredArgsConstructor
+public class FileController {
+
+  private final FileService fileService;
+
+  @PostMapping
+  public ResponseEntity<UpdateFileResponseDto> uploadFile(
+      @AuthenticationPrincipal final User user, final MultipartFile file) {
+    try {
+      final LocalDateTime now = LocalDateTime.now();
+      final long userId = Long.parseLong(user.getUsername());
+      final FileResult result = fileService.uploadFile(file.getBytes(), userId, now);
+
+      return ResponseEntity.ok(new UpdateFileResponseDto(result.id(), result.url()));
+    } catch (IOException e) {
+      return ResponseEntity.internalServerError().build();
+    }
+  }
+}
---
+++ perfume-api/src/main/java/io/perfume/api/file/application/port/in/dto/FileResult.java
@@ -0,0 +1,3 @@
+package io.perfume.api.file.application.port.in.dto;
+
+public record FileResult(Long id, String url) {}
