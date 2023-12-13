bbb2b8c2491e739b252fa31b50f11b90c34d08a8
oliviarla <imoliviarla@gmail.com>
2023-12-13 23:05:04 +0900

    feat: 브랜드 목록 조회 기능 추가

--- perfume-api/src/main/java/io/perfume/api/brand/adapter/out/persistence/brand/BrandQueryPersistenceAdapter.java
+++ perfume-api/src/main/java/io/perfume/api/brand/adapter/out/persistence/brand/BrandQueryPersistenceAdapter.java
@@ -6,6 +6,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import io.perfume.api.base.PersistenceAdapter;
 import io.perfume.api.brand.application.port.out.BrandQueryRepository;
 import io.perfume.api.brand.domain.Brand;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
@@ -25,5 +26,14 @@ public class BrandQueryPersistenceAdapter implements BrandQueryRepository {
             .fetchOne();
 
     return Optional.ofNullable(brandMapper.toDomain(result));
+  }
+
+  @Override
+  public List<Brand> findAll() {
+    List<BrandEntity> result = jpaQueryFactory.selectFrom(brandEntity)
+        .where(brandEntity.deletedAt.isNull())
+        .fetch();
+
+    return result.stream().map(brandMapper::toDomain).toList();
   }
 }
--- perfume-api/src/main/java/io/perfume/api/brand/application/port/in/FindBrandUseCase.java
+++ perfume-api/src/main/java/io/perfume/api/brand/application/port/in/FindBrandUseCase.java
@@ -2,9 +2,12 @@ package io.perfume.api.brand.application.port.in;
 
 import io.perfume.api.brand.application.port.in.dto.BrandForPerfumeResult;
 import io.perfume.api.brand.application.port.in.dto.BrandResult;
+import java.util.List;
 
 public interface FindBrandUseCase {
   BrandResult findBrandById(Long id);
 
   BrandForPerfumeResult findBrandForPerfume(Long id);
+
+  List<BrandResult> findAll();
 }
