bd7d9bab9e1523a60a90dbd8843e8ec8a48e0725
Big-Cir97 <eodnjs01477@gmail.com>
2023-12-13 12:17:58 +0900

    feat: 브랜드 매거진 조회 API 추가

--- perfume-api/src/main/java/io/perfume/api/brand/adapter/in/http/MagazineController.java
+++ perfume-api/src/main/java/io/perfume/api/brand/adapter/in/http/MagazineController.java
@@ -1,9 +1,14 @@
 package io.perfume.api.brand.adapter.in.http;
 
+import dto.ui.CursorResponse;
 import io.perfume.api.brand.adapter.in.http.dto.CreateMagazineRequestDto;
 import io.perfume.api.brand.adapter.in.http.dto.CreateMagazineResponseDto;
+import io.perfume.api.brand.adapter.in.http.dto.GetMagazineRequestDto;
+import io.perfume.api.brand.adapter.in.http.dto.GetMagazinesResponseDto;
 import io.perfume.api.brand.application.port.in.CreateMagazineUseCase;
+import io.perfume.api.brand.application.port.in.GetMagazineUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +22,26 @@ import java.time.LocalDateTime;
 public class MagazineController {
 
   private final CreateMagazineUseCase createMagazineUsecase;
+
+  private final GetMagazineUseCase getMagazineUseCase;
+
+  // TODO : Test, restDocs 작성
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping
+  public ResponseEntity<CursorResponse<GetMagazinesResponseDto, Long>> getMagazines(
+          @PathVariable("id") Long brandId,
+          GetMagazineRequestDto request
+  ) {
+    final var magazines = getMagazineUseCase.getMagazines(request.toCommand(brandId));
+    final var responseItems = magazines.getItems().stream().map(GetMagazinesResponseDto::from).toList();
+    return ResponseEntity.ok(
+            CursorResponse.of(
+                    responseItems,
+                    magazines.hasNext(),
+                    magazines.hasPrevious(),
+                    magazines.getFirstCursor().id(),
+                    magazines.getLastCursor().id()));
+  }
 
   @PreAuthorize("isAuthenticated()") // and hasRole('ADMIN')
   @PostMapping
