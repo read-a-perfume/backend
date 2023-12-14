741cdf392412e653ce1c66be37ebcbd928ece811
oliviarla <imoliviarla@gmail.com>
2023-12-14 23:46:04 +0900

    feat: 브랜드 id 필드 같이 반환

--- perfume-api/src/main/java/io/perfume/api/brand/application/port/in/dto/BrandResult.java
+++ perfume-api/src/main/java/io/perfume/api/brand/application/port/in/dto/BrandResult.java
@@ -3,8 +3,8 @@ package io.perfume.api.brand.application.port.in.dto;
 import io.perfume.api.brand.domain.Brand;
 import io.perfume.api.file.domain.File;
 
-public record BrandResult(String name, String story, String thumbnail) {
+public record BrandResult(Long id, String name, String story, String thumbnail) {
   public static BrandResult of(Brand brand, File file) {
-    return new BrandResult(brand.getName(), brand.getStory(), file.getUrl());
+    return new BrandResult(brand.getId(), brand.getName(), brand.getStory(), file.getUrl());
   }
 }
