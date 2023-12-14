da9df2879e4912158c118055efd2b852565bb635
oliviarla <imoliviarla@gmail.com>
2023-12-14 23:48:35 +0900

    test: 테스트 코드 추가

--- perfume-api/src/test/java/io/perfume/api/brand/adapter/out/persistence/BrandQueryPersistenceAdapterTest.java
+++ perfume-api/src/test/java/io/perfume/api/brand/adapter/out/persistence/BrandQueryPersistenceAdapterTest.java
@@ -1,8 +1,10 @@
 package io.perfume.api.brand.adapter.out.persistence;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.perfume.api.brand.adapter.out.persistence.brand.BrandEntity;
 import io.perfume.api.brand.adapter.out.persistence.brand.BrandMapper;
@@ -12,6 +14,7 @@ import io.perfume.api.brand.domain.Brand;
 import io.perfume.api.configuration.TestQueryDSLConfiguration;
 import jakarta.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,7 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 @ActiveProfiles("test")
 @Import({BrandQueryPersistenceAdapter.class, BrandMapper.class, TestQueryDSLConfiguration.class})
 @DataJpaTest
-public class BrandQueryPersistenceAdapterTest {
+class BrandQueryPersistenceAdapterTest {
 
   @Autowired private EntityManager em;
   @Autowired private BrandQueryRepository brandQueryRepository;
@@ -32,7 +35,7 @@ public class BrandQueryPersistenceAdapterTest {
   @Test
   @DisplayName("Brand 1건을 조회한다.")
   @Transactional
-  public void findByBrandId() {
+  void findByBrandId() {
     // given
     String brandName = "brand_name1";
     String brandStory = "story1";
@@ -58,7 +61,7 @@ public class BrandQueryPersistenceAdapterTest {
   @Test
   @DisplayName("제거된 Brand는 조회할 수 없다.")
   @Transactional
-  public void findByBrandId_NotFound() {
+  void findByBrandId_NotFound() {
     // given
     String brandName = "brand_name1";
     String brandStory = "story1";
@@ -80,5 +83,24 @@ public class BrandQueryPersistenceAdapterTest {
 
     // then
     assertFalse(optionalBrand.isPresent());
+  }
+
+  @Test
+  @DisplayName("Brand를 모두 조회한다.")
+  void findAll() {
+    for (int i = 0; i < 10; i++) {
+      BrandEntity brandEntity =
+          BrandEntity.builder().name("브랜드" + i).story("브랜드 스토리" + i).thumbnailId((long) i).build();
+      em.persist(brandEntity);
+    }
+    em.flush();
+    em.clear();
+
+    // when
+    List<Brand> brands = brandQueryRepository.findAll();
+
+    // then
+    assertEquals(10, brands.size());
+    assertTrue(brands.stream().allMatch(brand -> brand.getDeletedAt() == null));
   }
 }
