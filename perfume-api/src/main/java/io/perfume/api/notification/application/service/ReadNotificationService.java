f2f9d0f0bd1441bb2fdc1b03a82b33fe1b3e52ba
Big-Cir97 <eodnjs01477@gmail.com>
2023-12-18 02:47:36 +0900

    feat: 알림 읽기 서비스 구현

---
+++ perfume-api/src/main/java/io/perfume/api/notification/application/service/ReadNotificationService.java
@@ -0,0 +1,42 @@
+package io.perfume.api.notification.application.service;
+
+import io.perfume.api.notification.application.exception.NotFoundNotificationException;
+import io.perfume.api.notification.application.exception.ReadNotificationPermissionDeniedException;
+import io.perfume.api.notification.application.port.in.ReadNotificationUseCase;
+import io.perfume.api.notification.application.port.out.notification.NotificationQueryRepository;
+import io.perfume.api.notification.application.port.out.notification.NotificationRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ReadNotificationService implements ReadNotificationUseCase {
+
+  private final NotificationQueryRepository notificationQueryRepository;
+
+  private final NotificationRepository notificationRepository;
+
+  public ReadNotificationService(
+      NotificationQueryRepository notificationQueryRepository,
+      NotificationRepository notificationRepository) {
+    this.notificationQueryRepository = notificationQueryRepository;
+    this.notificationRepository = notificationRepository;
+  }
+
+  @Override
+  @Transactional
+  public void read(Long userId, Long notificationId, LocalDateTime now) {
+    var notification =
+        notificationQueryRepository
+            .findById(notificationId)
+            .orElseThrow(() -> new NotFoundNotificationException(notificationId));
+
+    if (!notification.isOwner(userId)) {
+      throw new ReadNotificationPermissionDeniedException(userId, notificationId);
+    }
+
+    notification.readNotification();
+    notificationRepository.save(notification);
+  }
+}
