a83e6005efd3d2832fae21b7ab5e8cee339985d8
oliviarla <imoliviarla@gmail.com>
2023-12-25 22:40:58 +0900

    feat: 이메일 인증 smtp 연동

--- perfume-api/src/main/java/io/perfume/api/common/config/MailSenderConfiguration.java
+++ perfume-api/src/main/java/io/perfume/api/common/config/MailSenderConfiguration.java
@@ -1,15 +1,61 @@
 package io.perfume.api.common.config;
 
+import java.util.Properties;
 import mailer.MailSender;
-import mailer.impl.SESMailSender;
+import mailer.impl.MailSenderImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailSenderConfiguration {
+  @Value("${spring.mail.host}")
+  private String host;
+
+  @Value("${spring.mail.port}")
+  private int port;
+
+  @Value("${spring.mail.username}")
+  private String username;
+
+  @Value("${spring.mail.password}")
+  private String password;
+
+  @Value("${spring.mail.properties.mail.smtp.auth}")
+  private String auth;
+
+  @Value("${spring.mail.properties.mail.smtp.timeout}")
+  private String timeout;
+
+  @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+  private String starttls;
 
   @Bean
   public MailSender mailSender() {
-    return new SESMailSender();
+    String fromEmail = username + host.split("\\.")[1];
+    return new MailSenderImpl(javaMailSender(), fromEmail);
+  }
+
+  @Bean
+  public JavaMailSender javaMailSender() {
+    JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+    javaMailSender.setHost(host);
+    javaMailSender.setPort(port);
+    javaMailSender.setUsername(username);
+    javaMailSender.setPassword(password);
+    javaMailSender.setJavaMailProperties(getMailProperties());
+
+    return javaMailSender;
+  }
+
+  private Properties getMailProperties() {
+    Properties properties = new Properties();
+    properties.setProperty("mail.smtp.auth", auth);
+    properties.setProperty("mail.smtp.timeout", timeout);
+    properties.setProperty("mail.smtp.starttls.enable", starttls);
+    return properties;
   }
 }
--- perfume-core/build.gradle.kts
+++ perfume-core/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
     implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-mail:3.2.1")
 }
 
 tasks.bootJar {
