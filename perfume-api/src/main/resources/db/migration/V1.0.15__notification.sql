ce68b759e95901a8cc2c6f184f240219277645cf
Byeongju, Shin <404err@naver.com>
2023-12-30 23:31:05 +0900

    Revert "docs: magazine script 추가"
    This reverts commit 546e7f3f

--- perfume-api/src/main/resources/db/migration/V1.0.15__notification.sql
+++ perfume-api/src/main/resources/db/migration/V1.0.15__notification.sql
@@ -1,13 +1,13 @@
 create table notification
 (
-    id                bigint auto_increment not null,
-    content           VARCHAR(255),
-    redirect_url      VARCHAR(255),
-    receive_user_id   bigint not null,
+    id         bigint auto_increment not null,
+    content VARCHAR(255),
+    redirect_url VARCHAR(255),
+    receive_user_id bigint not null,
     notification_type VARCHAR(50) not null,
-    is_read           BOOLEAN not null,
-    created_at        datetime not null,
-    updated_at        datetime not null,
-    deleted_at        datetime null,
+    is_read BOOLEAN not null,
+    created_at datetime not null,
+    updated_at datetime not null,
+    deleted_at datetime null,
     constraint pk_notification primary key (id)
 )
